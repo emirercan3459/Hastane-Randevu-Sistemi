b0de8f84d2dec95c5bcd37b3eb8378c10902aa0a
Emir <emirercan3459@gmail.com>
2025-05-28 10:28:06 +0300

    Commit Message

--- src/main/java/com/emirercan/HastaneRandevuSistemi/Main.java
+++ src/main/java/com/emirercan/HastaneRandevuSistemi/Main.java
@@ -7,6 +7,6 @@ import javax.swing.*;
 public class Main extends JFrame {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(MainGUI::new);
+        new MainGUI();
     }
 }
